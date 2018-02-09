008de9d06579e1efc6bc4a535faa83277d1294b7
Tchun Chao <chaot_xp@live.fr>
2018-02-09 16:16:16 -0500

    Fin du cours + correction erreur db.collection

--- 01_apps.js
+++ 01_apps.js
@@ -2,24 +2,31 @@ const express = require('express');
 const app = express();
 const fs = require('fs');
 app.use(express.static('public'));
-
+const MongoClient = require('mongodb').MongoClient;
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended:true}));
 /* on associe le moteur de vue au module «ejs» */
 app.set('view engine', 'ejs'); // générateur de template
 
-app.get('/', function (req, res) {
-   fs.readFile( __dirname + "/public/data/" + "membres.txt", 
-        'utf8',
-        (err, data) => {
-        	if (err) { return console.error(err);}
-        	console.log( data );
-        	let resultat = JSON.parse("[" + data + "]");           
-  res.render('gabarit.ejs', {adresses: resultat})
-  });
+var db // variable qui contiendra le lien sur la BD
+MongoClient.connect('mongodb://127.0.0.1:27017/carnet_adresse', (err, database) => {
+ if (err) return console.log(err)
+ db = database
+// lancement du serveur Express sur le port 8081
+ app.listen(8081, () => {
+ console.log('connexion à la BD et on écoute sur le port 8081')
+ })
 })
 
-let server = app.listen(8081, function () {
-   let host = server.address().address
-   let port = server.address().port
-   
-   console.log("Example app listening at http://%s:%s", host, port)
+app.get('/', (req, res) => {
+ console.log('la route route get / = ' + req.url)
+ console.log(db);
+ let cursor = db.collection('adresse')
+                .find().toArray(function(err, resultat){
+ if (err) return console.log(err)
+ 	console.log(JSON.stringify(resultat));
+ // transfert du contenu vers la vue index.ejs (renders)
+ // affiche le contenu de la BD
+ res.render('gabarit.ejs', {adresses: resultat})
+ })
 })
