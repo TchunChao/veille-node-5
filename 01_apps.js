7022c4aa3ad07c158dd032d71be5730374e3cb61
Tchun Chao <chaot_xp@live.fr>
2018-02-09 13:32:38 -0500

    Correction de .gitignore

--- 01_apps.js
+++ 01_apps.js
@@ -1,15 +1,25 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+const fs = require('fs');
+app.use(express.static('public'));
 
 /* on associe le moteur de vue au module «ejs» */
 app.set('view engine', 'ejs'); // générateur de template
+
 app.get('/', function (req, res) {
-   res.send('Hello World');
+   s.readFile( __dirname + "/public/data/" + "membres.txt", 
+        'utf8',
+        (err, data) => {
+        	if (err) { return console.error(err);}
+        	console.log( data );
+        	let resultat = JSON.parse("[" + data + "]");           
+  res.render('gabarit.ejs', {addresses: resultat})
+  });
 })
 
-var server = app.listen(8081, function () {
-   var host = server.address().address
-   var port = server.address().port
+let server = app.listen(8081, function () {
+   let host = server.address().address
+   let port = server.address().port
    
    console.log("Example app listening at http://%s:%s", host, port)
 })
