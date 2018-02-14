633061525e507440d1ae92533dabe223d1946518
Tchun Chao <chaot_xp@live.fr>
2018-02-14 11:47:11 -0500

    Rendu a affichage du carnet_adresse

--- 01_apps.js
+++ 01_apps.js
@@ -8,25 +8,24 @@ app.use(bodyParser.urlencoded({extended:true}));
 /* on associe le moteur de vue au module «ejs» */
 app.set('view engine', 'ejs'); // générateur de template
 
-var db // variable qui contiendra le lien sur la BD
+let db // variable qui contiendra le lien sur la BD
 MongoClient.connect('mongodb://127.0.0.1:27017/carnet_adresse', (err, database) => {
  if (err) return console.log(err)
- db = database
+ db = database.db('carnet_adresse')
 // lancement du serveur Express sur le port 8081
  app.listen(8081, () => {
  console.log('connexion à la BD et on écoute sur le port 8081')
  })
 })
 
-app.get('/', (req, res) => {
+app.get('/adresse', (req, res) => {
  console.log('la route route get / = ' + req.url)
- console.log(db);
- let cursor = db.collection('adresse')
+ 
+ var cursor = db.collection('adresse')
                 .find().toArray(function(err, resultat){
  if (err) return console.log(err)
- 	console.log(JSON.stringify(resultat));
  // transfert du contenu vers la vue index.ejs (renders)
  // affiche le contenu de la BD
- res.render('gabarit.ejs', {adresses: resultat})
- })
+ res.render('gabarit.ejs', {adresse: resultat})
+ }) 
 })
